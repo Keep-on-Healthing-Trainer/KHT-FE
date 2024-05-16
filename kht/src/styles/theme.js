f42668069b4e0ddac393378a93fd742acc1a81c5
downpour07 <choiheeyoung10@gmail.com>
2024-05-16 17:34:30 +0900

    📑 :: style set

---
+++ kht/src/styles/theme.js
@@ -0,0 +1,7 @@
+export const color = {
+    Blue: ["#E7EEF6","#CEDCEC" ,"#B6CBE3", "#9EBADA", "#85A8D1", "#6D97C7", "#5585BE", "#2463AC", "#0C51A2", "#0A386E", "#0075FF"],
+    White: "#FFFFFF",
+    Gray: ["#FAFAFA", "#F4F4F5", "#E4E4E7", "#D4D4D8", "#A1A1AA", "#3F3F46"],
+    Black: "#000",
+    Red: "#F5290A"
+};
