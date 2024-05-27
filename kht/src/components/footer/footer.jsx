b40c5ee008f2febd8d9a8d52ba35b5bc2210e8cc
downpour07 <choiheeyoung10@gmail.com>
2024-05-27 21:59:12 +0900

    📑 :: footer 추가

---
+++ kht/src/components/footer/footer.jsx
@@ -0,0 +1,14 @@
+import { styled } from "styled-components";
+
+const Footer = () => {
+    return (
+      <></>
+    );
+  };
+
+export const Background = styled.div`
+  width: 100vw;
+  height: 6vh;
+`;
+
+export default Footer;
