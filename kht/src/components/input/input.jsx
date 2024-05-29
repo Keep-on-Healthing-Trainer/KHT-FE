db1713fdad7a0f91e7a29d83309ab947c847e830
downpour07 <choiheeyoung10@gmail.com>
2024-05-29 19:03:09 +0900

    📝 :: input 수정

---
+++ kht/src/components/input/input.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { styled } from "styled-components";
+import { color } from "../../styles/theme";
+import { breakpoints } from "../../styles/device";
+
+const Input = ( props ) => {
+    return (
+        <Div>
+            <Text>{props.innerText}</Text>
+            <InputText type={props.name} />
+        </Div>
+    );
+};
+
+export const Div = styled.div`
+    margin: 3vh 5vw;
+`;
+
+export const Text = styled.div`
+    font-size: 0.9em;
+    font-weight: 600;
+`;
+
+export const InputText = styled.input`
+    width: 30vw;
+    height: 6vh;
+    font-size: 1em;
+    border: none;
+    background-color: ${color.Gray[1]};
+    border-radius: 10px;
+    padding: 0 10px;
+
+    @media only screen and (max-width: ${breakpoints.medium}) {
+        width: 500px;
+    }
+`;
+
+export default Input;
