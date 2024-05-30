d78737506ce7ba17647e2a2c00324abb57ff2b73
downpour07 <choiheeyoung10@gmail.com>
2024-05-30 20:19:53 +0900

    📝 :: router 수정

--- kht/src/App.js
+++ kht/src/App.js
@@ -1,20 +1,31 @@
 import React from "react";
 import {
   BrowserRouter,
-  BrowserRouter as Router,
   Route,
   Routes,
 } from "react-router-dom";
+
+import BeforePage from "./pages/before";
+import LoginPage from "./pages/login";
+import SignupPage from "./pages/signup";
+import MainPage from "./pages/main";
+import SearchPage from "./pages/search";
+
+import BeforeLayout from "./navigation/BeforeLayout";
+import MainLayout from "./navigation/MainLayout";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Header />}>
+        <Route element={<BeforeLayout />}>
           <Route path="/" element={<BeforePage />} />
-          <Route path="/main" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage/>} />
+        </Route>
+        <Route element={<MainLayout />}>
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/search" element={<SearchPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
