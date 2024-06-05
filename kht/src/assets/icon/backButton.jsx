6a4d9b83a14f52d0c573172276ac5e973e43b3a8
downpour07 <choiheeyoung10@gmail.com>
2024-06-05 11:56:12 +0900

    📑 :: icon 추가

---
+++ kht/src/assets/icon/backButton.jsx
@@ -0,0 +1,10 @@
+const BackButton = (props) => {
+    return (
+<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="25" cy="25" r="24.5" stroke="#A1A1AA"/>
+<path d="M13 25L31 14.6077V35.3923L13 25Z" fill="#A1A1AA"/>
+</svg>
+    );
+  };
+  
+export default BackButton;
---
+++ kht/src/assets/icon/nextButton.jsx
@@ -0,0 +1,10 @@
+const NextButton = (props) => {
+    return (
+<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="25" cy="25" r="24.5" transform="rotate(-180 25 25)" stroke="#A1A1AA"/>
+<path d="M37 25L19 35.3923L19 14.6077L37 25Z" fill="#A1A1AA"/>
+</svg>
+    );
+  };
+  
+export default NextButton;
